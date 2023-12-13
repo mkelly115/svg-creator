c23e76e3b5c1130a23532e44b8418522db62a59d
michael <mikek222@gmail.com>
2023-12-13 12:47:19 +0300

    fixed up logo.svg text placement, reworked validation to account for all cases, skipped testing given new params, completed shapes file and cleaned up index.js

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 const inquirer = require('inquirer');
+const colorList = require('color-name-list');
 const fs = require('fs');
 const generateShapes = require('./lib/shapes.js'); 
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+
+const validateColor = (input) => {
+
+    const isValidColorName = colorList.some((color) => color.name.toLowerCase() === input.toLowerCase());
+    const isValidHexCode = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input);
+
+    return isValidColorName || isValidHexCode ? true : 'Please enter a valid color or hexadecimal code';
+};
+
+const validateChar = (input) => {
+    if (input.length !== 3) {
+        return 'Please enter exactly 3 characters';
+    }
+    return true;
+};
 
 const questions = [
     {
-        type: 'maxlength-input',
+        type: 'input',
         message: 'Please select 3 Characters',
         name: 'char',
-        maxLength: '3',
+        validate: validateChar,
     },
     {
         type: 'input',
         message: 'Please select character text color',
         name: 'charColor',
+        validate: validateColor,
     },
     {
-        type:"list",
-        message: "Please select a shape",
-        choices: ["circle","triangle","square"],
-        name: "shape"
+        type: 'list',
+        message: 'Please select a shape',
+        choices: ['circle', 'triangle', 'square'],
+        name: 'shape',
     },
     {
         type: 'input',
         message: 'Please select a shape color',
         name: 'shapeColor',
+        validate: validateColor,
     },
 ];
 
 function writeToFile(fileName, content) {
     fs.writeFileSync(fileName, content, 'utf-8');
-    console.log(`${fileName} created successfully!`);
+    console.log(`Generated ${fileName}`);
 }
 
 function init() {
     inquirer.prompt(questions).then((answers) => {
         const userSvg = generateShapes(answers); 
-        writeToFile("output.svg", userSvg); 
+        writeToFile("logo.svg", userSvg); 
 
     });
 }
 
 init()
+
+
